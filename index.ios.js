be0c34d61723a397f0a451dbd4f27ebca24dd59c
Gavin Wolf <gavinwolf@Gavins-MacBook-Pro.local>
2017-08-14 09:55:19 -0400

    Add textbox and switch

--- index.ios.js
+++ index.ios.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import {AppRegistry, StyleSheet, Text, View} from 'react-native';
 
-import Component2 from './app/components/Component2/Component2';
+import Component3 from './app/components/Component3/Component3';
 
 export default class myReactNativeApp extends Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>
-          <Component2 />
+          <Component3 />
         </Text>
       </View>
     );
